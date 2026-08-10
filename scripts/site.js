babebb569e64e90333870708306be2fb219ad5eb
Reis Mahnic <reismahnic@gmail.com>
2026-08-09 21:53:38 -0700

    Updated Fogtown youtube video

--- scripts/site.js
+++ scripts/site.js
@@ -42,7 +42,7 @@ $('.loop').hover(function(){
 $('.fogtown').click (function(){
   $(this).hide();
   $("#fogtown-video").show();
-  $("#fogtown-video").html('<iframe src="https://www.youtube.com/watch?v=M-aW-UXlz38" width="100%" height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+  $("#fogtown-video").html('<iframe src="https://www.youtube.com/embed/M-aW-UXlz38?si=AaWpzhN21NfQ8eAl" width="100%" height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
 })
 $('.river').click (function(){
   $(this).hide();
